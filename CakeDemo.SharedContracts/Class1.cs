a32834a26ab02a496367ceb7dfff617aab64c826
Michael Samorokov <mykhailo.samorokov@betssongroup.com>
2017-06-12 18:01:10 +0200

    Added demo library

---
+++ CakeDemo.SharedContracts/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeDemo.SharedContracts
+{
+    public class CakeDemoData
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
